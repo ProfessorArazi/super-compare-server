054f68474354ee40b330742d36f0589ff31319ea
ProfessorArazi <amitarazi123456@gmail.com>
2024-10-28 00:10:11 +0200

    unit resolution

--- src/Models/Price.js
+++ src/Models/Price.js
@@ -15,6 +15,11 @@ const PriceSchema = new mongoose.Schema({
         type: Number,
         required: true,
     },
+    unitResolution: {
+        type: Number,
+        default: 1,
+    },
+
     discount: {
         type: Number,
         default: 0,
