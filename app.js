4e85cd0b4fc53f5d721247fbdea4c2d113cbe442
ProfessorArazi <amitarazi123456@gmail.com>
2024-10-29 22:00:55 +0200

    increase json size limit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const favoritesRouter = require("./src/routers/favorites");
 const { verifyToken } = require("./src/middlewares/authenticate");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 const corsOptions = {
     origin: "*",
